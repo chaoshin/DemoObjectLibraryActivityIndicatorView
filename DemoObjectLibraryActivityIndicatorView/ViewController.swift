9e9afc629e5cc6c00fca12fb77f246f39d54a956
Chao Shin <chao.shin@gmail.com>
2018-03-17 15:43:40 +0800

    簡易說明Xcode中Object Library的Activity Indicator View-Demo Source Code

--- DemoObjectLibraryActivityIndicatorView/ViewController.swift
+++ DemoObjectLibraryActivityIndicatorView/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mySwitchAction(_ sender: UISwitch) {
+        if (sender as AnyObject).isOn == true {    // 判斷使用者選擇是開還是關
+            myActivityIndicator.startAnimating() // 啟動ActivityIndicator的Animating
+            
+        }else {
+            myActivityIndicator.stopAnimating() // 停止ActivityIndicator的Animating
+            
+        }
+    }
+    
 
+    
 }
 
